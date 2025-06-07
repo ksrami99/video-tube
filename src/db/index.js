074caeb4ef87644d5e87959fd672d3818216865f
Rami Karan <99781533+ksrami99@users.noreply.github.com>
2025-06-07 15:47:18 +0530

    feat: set up database connection and environment configuration
    - Updated package.json to include dotenv, express, and mongoose dependencies.
    - Modified the dev script to use dotenv and experimental JSON modules.
    - Added constants.js to define the database name.
    - Created db/index.js to handle MongoDB connection using mongoose.
    - Updated index.js to configure dotenv and establish a database connection on startup.

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = () => {
+  mongoose
+    .connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+    .then((connctionInstance) => {
+      console.log(`DB Connected! DB Host ${connctionInstance.connection.host}`);
+    })
+    .catch((error) => {
+      console.log("ERROR " + error);
+      process.exit(1);
+    });
+};
+
+
+export default connectDB;
